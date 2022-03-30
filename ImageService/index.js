16d0dc223e3110ad680980377c86c370eabeae38
Adam Cyffka <adam.cyffka@epitech.eu>
2022-03-30 17:47:39 +0200

    Add image viewer

--- ImageService/index.js
+++ ImageService/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path')
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./images");
+    const path = "./uploads/"
+    fs.mkdirSync(path, { recursive: true })
+    return cb(null, path)
   },
   filename: (req, file, cb) => {
     cb(null, uuidv4() + path.extname(file.originalname));
@@ -21,19 +23,13 @@ const upload = multer({ storage: fileStorageEngine });
 
 app.post("/upload_files", upload.single("image"), (req, res) => {
   console.log(req.file);
-  res.send("File uploaded");
+  res.send({ status: 'Uploaded', token: req.file.filename });
 });
 
-app.get("/show_image", (req, res) => {
-  let ddzzdzd = "images/1b0b0b7d-e887-4534-8e63-d9091552d9f9.png";
-  var img = new Image();
-  img.src = ddzzdzd;
-  document.body.appendChild(img);
+app.get("/:token", (req, res) => {
+  let filepath = "/uploads/" + req.params.token;
+  res.sendFile(__dirname + filepath);
 });
-
-function searchImages() {
-
-}
 
 app.listen(process.env.DOCKER_PORT, () => {
   console.log(`App start on port ` + process.env.DOCKER_PORT);
