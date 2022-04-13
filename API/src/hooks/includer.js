c2f0dd3f346f66e53df69e94e3cd772b5b730e86
Valipss <valentin1.masson@epitech.eu>
2022-04-13 15:24:19 +0200

    comments included

--- API/src/hooks/includer.js
+++ API/src/hooks/includer.js
@@ -15,17 +15,19 @@ module.exports = (context) => {
     // in the actual database query
     delete context.params.query.$include;
 
-    function loopIncluder(toLoopOn) {
+    function loopIncluder(toLoopOn, currentScope = "") {
         var toFill = [];
         toLoopOn.forEach(toInclude => {
+            console.log(toInclude);
             if (isJSON(toInclude)) {
                 const nested = JSON.parse(toInclude);
+                // console.log(nested);
 
                 if (nested.name && nested.include && nested.limit && nested.skip && Array.isArray(nested.include)) {
                     if (nested.name in context.service.Model.associations) {
                         toFill.push({
                             model: context.app.service(nested.name).Model,
-                            include: loopIncluder(nested.include),
+                            include: loopIncluder(nested.include, nested.name),
                             limit: nested.limit,
                             offset: nested.skip
                         });
@@ -34,25 +36,46 @@ module.exports = (context) => {
                     if (nested.name in context.service.Model.associations) {
                         toFill.push({
                             model: context.app.service(nested.name).Model,
-                            include: loopIncluder(nested.include)
+                            include: loopIncluder(nested.include, nested.name),
                         });
                     }
                 }
             } else {
-                if (toInclude in context.service.Model.associations || toInclude.slice(0, -1) in context.service.Model.associations) {
-                    toFill.push({
-                        model: context.app.service(toInclude).Model,
-                    });
+                var includes = toInclude.split(":");
+                if (includes[0] in context.service.Model.associations || includes[0].slice(0, -1) in context.service.Model.associations) {
+                    if (includes[1]) {
+                        toFill.push({
+                            model: context.app.service(includes[0]).Model,
+                            as: includes[1]
+                        });
+                    } else {
+                        toFill.push({
+                            model: context.app.service(includes[0]).Model,
+                        });
+                    }
+                } else if (includes[0] in context.app.service(currentScope).Model.associations || includes[0].slice(0, -1) in context.app.service(currentScope).Model.associations) {
+                    if (includes[1]) {
+                        toFill.push({
+                            model: context.app.service(includes[0]).Model,
+                            as: includes[1]
+                        });
+                    } else {
+                        toFill.push({
+                            model: context.app.service(includes[0]).Model,
+                        });
+                    }
                 }
             }
         });
-
+        console.log("INCLUDE\n")
+        console.log(toFill);
+        console.log("\n");
         return toFill;
     };
 
     if(Array.isArray($include)) {
     
-        let include = loopIncluder($include);
+        let include = loopIncluder($include, "");
         if (include) {
             context.params.sequelize = context.params.sequelize || {};
             merge(context.params.sequelize, {
